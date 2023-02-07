879f010b6c60db5fe0686d17e425f693b0e029b6
jigs074 <90673637+jigs074@users.noreply.github.com>
2023-02-06 23:20:59 -0500

    Changes made

--- WeatherApplication/Form1.cs
+++ WeatherApplication/Form1.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Newtonsoft.Json;
+using System.Net; 
 namespace WeatherApplication
 {
     public partial class Form1 : Form
@@ -15,6 +16,18 @@ namespace WeatherApplication
         public Form1()
         {
             InitializeComponent();
+        }
+        string APIkey = "b353092d473f54c232544798a31178f3";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = string.Format("https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}");
+
         }
     }
 }
